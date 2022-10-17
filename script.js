dcec7bf4a95d7d6d4421d336a87c604c16bfa40c
NoamSeb <noam.sebahoun@gmail.com>
2022-10-17 20:09:36 +0200

    add new features
    :sparkles: add a tribute section with all the names.
    
    :rocket: add legend to the graph for a best reading.

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ Promise.all([
     initApp(data16, data17, data18, data19, data20, data21, data22);
 });
 
+function random(min, max) {
+    let n;
+    do {
+        n = Math.round(Math.random() * (max - min) + min);
+    } while (n === 0);
+    return n;
+}
+
 function initApp(data16, data17, data18, data19, data20, data21, data22) {
     //correction du nombre de victime en 2016 après vérification sur internet
     //stockage de données dans un tableau
@@ -44,13 +52,52 @@ function initApp(data16, data17, data18, data19, data20, data21, data22) {
         .style("stroke-width", "4")
     d3.select
 
-    // axis(d3.scaleLog().domain([1e0, 1e6]))
-    //     .ticks(2)
-    //     .render()
-    //     .style("color", "white")
-    var myArray = [123, data16.length, data17.length, data18.length, data19.length, data20.length, data21.length, data22.length];
-    var a = ArrayAvg(myArray);
-    console.log(a)
+    const axesB = d3.axisBottom(d3.scaleLinear().domain([2016, 2022]).range([0, 700]))
+        .ticks(7)
+        .tickFormat(d3.format('d'));
+    const axesL = d3.axisLeft(d3.scaleLinear().domain([0, 300]).range([0, -300]))
+        .ticks(7)
+
+
+    d3.select("#dessin")
+        .append("g")
+        .call(axesB)
+        .style("font-size", "1vw")
+
+    d3.select("#dessin")
+        .append("g")
+        .call(axesL)
+        .style("font-size", "1vw")
+
+
+    let myArray = [123, data16.length, data17.length, data18.length, data19.length, data20.length, data21.length, data22.length];
+    let a = ArrayAvg(myArray);
+    console.log(Math.ceil(a))
+
+    // code pour le nuage de prénom
+
+    let allArray = [data16, data17, data18, data19, data20, data21, data22];
+    let annee = 2016;
+
+    for (let array of allArray) {
+        d3.select("#nuage")
+            .style("position", "relative")
+            .selectAll("ijhkjhkjh")
+            .data(array)
+            .enter()
+            .append("div")
+            .attr("class", "prenom " + annee)
+            .text(d => d.name.split(/[ ,]/)[0])
+            .style("position", "relative")
+            .style("top", d => random(5, 95) + "%")
+            .style("left", d => random(5, 95) + "%")
+            .style("opacity", d => random(3, 8) / 10)
+        annee++
+    }
+
+    console.log(data16.name)
+
+
 }
 
 //Code pour map Leaflet
@@ -98,10 +145,12 @@ const media = document.querySelector("video");
 const btn = document.querySelector("button");
 const volume = document.querySelector("input");
 const progress = document.querySelector("progress");
+const pause = document.querySelector(".pauseDiv")
 
 function playPause(e) {
     if (media.paused == false) {
         media.pause()
+        pause.style.display = "block"
     } else {
         media.play()
     }
@@ -126,3 +175,33 @@ progress.addEventListener('click', (event) => {
     media.currentTime = event.offsetX;
     progress.value = (media.currentTime * 100 / media.duration)
 });
+
+// script pour le nuage de prénom
+
+// const anneeHommage = document.querySelector(".annee");
+
+// function changePrenom() {
+//     console.log("hello");
+//     document.querySelectorAll(".prenom").style.display = "none";
+
+//     if (document.querySelector(".prenom").classList.contains(n)) {
+//         document.querySelector(".prenom " + n).style.display = "block";
+//     }
+// }
+
+// anneeHommage.addEventListener("click", changePrenom);
+
+document.querySelectorAll('.annee').forEach(function(i) {
+    i.addEventListener('click', function(e) {
+
+        document.querySelectorAll(".prenom").forEach(function(pre) {
+
+
+            if (pre.classList.contains(e.target.getAttribute('id'))) {
+                pre.style.display = "block";
+            } else {
+                pre.style.display = "none";
+            }
+        })
+    })
+})
